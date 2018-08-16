e9319391600de5c42a505634b922ef59f7191694
Jakub Skwara <jakub.skwara.js@gmail.com>
2018-08-16 22:10:25 +0200

    Stworzenie klasy Gravity obsługujacej grawitacje

--- src/sample/objects/Bullet.java
+++ src/sample/objects/Bullet.java
@@ -4,7 +4,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Bullet extends GameObject {
+    private static final double MAX_GRAVITY_FACTOR = 15;
+
     public Bullet(){
         super(new Circle(5, 5, 5, Color.BROWN));
+        setMaxGravityFactor(MAX_GRAVITY_FACTOR);
     }
 }
--- src/sample/objects/Enemy.java
+++ src/sample/objects/Enemy.java
@@ -4,8 +4,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Enemy extends GameObject{
+    private static final double MAX_GRAVITY_FACTOR = 5;
     public Enemy() {
         super(new Circle(15, 15, 15, Color.RED));
+        setMaxGravityFactor(MAX_GRAVITY_FACTOR);
     }
 
 }
